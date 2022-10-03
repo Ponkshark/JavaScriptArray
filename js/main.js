c427b21e403d3fa79df28901bdc98e5f4f23b6f7
Ponkshark <110033071+Ponkshark@users.noreply.github.com>
2022-10-03 10:48:07 +0100

    - Removed console logging from development. - Added the meta name viewport tag to the html head. - Added a media query for small screen size scaling. - Added padding to the input and submit button. - Increased text size and changed font of the submit button text. - Added better email validation to the form to account for spaces after and before the email address. - Validation messages are now displayed on page rather than through popups.

--- js/main.js
+++ js/main.js
@@ -22,10 +22,13 @@ myArray = [];
 
 // Email address validation 
 function validateForm(inputText) {
-    var mailformat = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/ ;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
     let y = document.forms["contactForm"]["youremail"].value;
+    const $result = $('#result')
+    $result.text('')
     if (y==null || y == "") {
-        alert("Email field required");
+        $result.text("Email address required");
+        $result.css('color', 'red');
         return false;
     }
     if (inputText.value.match(mailformat)) {
@@ -40,12 +43,13 @@ function validateForm(inputText) {
         myArray.push(emailValue);
         myArray.push(imgUrl);
       }
-      console.log(myArray);
+      // console.log(myArray); was used during testing
       generateTable();
       getImage();
       return true;
     } else {
-      alert("You have entered an invalid email address!");
+      $result.text("You have entered an invalid email address!");
+      $result.css('color', 'red');
       document.contactForm.youremail.focus();
       return false;
     }
